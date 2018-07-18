fabe6cd955b047e27c48d5efdf728416a947ff5a
Michaela <minjeong.k@linecorp.com>
2018-07-18 15:35:00 +0900

    Convert to my_partition_manager
    Supports weekly/daily partition

--- README.md
+++ README.md
@@ -1,60 +1,81 @@
 # myPartitionManager
-mysql monthly partition manager
-- RANGE Partitioning 
+mysql partition manager
+- RANGE Partitioning : monthly, weekly, daily
   - based on the value of a TIMESTAMP column
   - partioning_key_function : Date or Time (default:UNIX_TIMESTAMP()) function
 - RANGE COLUMNS partitioning supports
   - based on a DATE column
   - partitioning_key_function : QUOTE
-## Usages
+## Usage
 ```
-Usage: ./monthly_partition_manager.sh -d <dbname> -t <tablename> [-x <expire_months>] [-a <add_months>] [-p <partition_prefix>] [-f <partitioning_key_function>]
+Usage: ./my_partition_manager.sh -d <dbname> -t <tablename> -u month|week|day [-x <expire_months>] [-a <add_months>] [-p <partition_prefix>] [-f <partitioning_key_function>]
 ```
 ## Example
-### RANGE partitioning
+### RANGE partitioning(weekly)
 ```
-[root@testhost:/mysql/admin/partition]# ./monthly_partition_manager.sh -d test -t Alogs -a 10 -x 2
+[root@host:/mysql/admin/partition]# ./my_partition_manager.sh -d test -t weeklogs -r week -a 3 -x 5
+------------------------------------------------------------------------------------------------------------
+-- Partition manager :: 2018-06-01 17:27:32
+--     Target DB    :: test
+--     Target Table :: weeklogs
+--     Range Type :: week
+------------------------------------------------------------------------------------------------------------
+Warning: Using a password on the command line interface can be insecure.
+ADD Partiton for 3 week(s)
+DROP Partition before 5 week(s)
+Warning: Using a password on the command line interface can be insecure.
+ADD PARTITION: p20180611, 1529247600
+ALTER TABLE `test`.`weeklogs` ADD PARTITION (PARTITION p20180611 VALUES LESS THAN (1529247600))
+Warning: Using a password on the command line interface can be insecure.
+DROP PARTITION: p20180416, 1524409200
+ALTER TABLE `test`.`weeklogs` DROP PARTITION p20180416
+Warning: Using a password on the command line interface can be insecure.
+```
+### RANGE partitioning(monthly)
+
+```
+[root@host:/mysql/admin/partition]# ./my_partition_manager.sh -d test -t monthlogs -r month -a 10 -x 2
 ------------------------------------------------------------------------------------------------------------
 -- Partition creating :: 2017-07-06 18:34:49
 --     Target DB    :: test
---     Target Table :: Alogs
+--     Target Table :: monthlogs
 ------------------------------------------------------------------------------------------------------------
 ADD Partiton for 10 month(s)
 DROP Partition before 3 month(s)
 ADD PARTITION: p201804, 1525100400
-ALTER TABLE test.Alogs ADD PARTITION (PARTITION p201804 VALUES LESS THAN (1525100400))
+ALTER TABLE test.monthlogs ADD PARTITION (PARTITION p201804 VALUES LESS THAN (1525100400))
 DROP PARTITION: p201704, 1493564400
-ALTER TABLE test.Alogs DROP PARTITION p201704
+ALTER TABLE test.monthlogs DROP PARTITION p201704
 ```
 ```
-[root@testhost:/mysql/admin/partition]#  sh /mysql/admin/partition/monthly_partition_manager.sh -d test -t Blogs -a 3 -f to_days
+[root@host:/mysql/admin/partition]#  ./my_partition_manager.sh -d test -t datetypelogs -r month -a 3 -f to_days
 ------------------------------------------------------------------------------------------------------------
 -- Partition manager :: 2017-09-28 16:53:05
 --     Target DB    :: test
---     Target Table :: Blogs
+--     Target Table :: datetypelogs
 ------------------------------------------------------------------------------------------------------------
 ADD Partiton for 3 month(s)
 Warning: Using a password on the command line interface can be insecure.
 ADD PARTITION: p201711, 737029
-ALTER TABLE test.Blogs ADD PARTITION (PARTITION p201711 VALUES LESS THAN (737029))
+ALTER TABLE test.datetypelogs ADD PARTITION (PARTITION p201711 VALUES LESS THAN (737029))
 Warning: Using a password on the command line interface can be insecure.
 ```
 ### RANGE COLUMNS partitioning
 ```
-[root@testhost:/mysql/admin/partition]# ./monthly_partition_manager.sh -d test -t Clogs -a 3 -x 3 -f quote
+[root@host:/mysql/admin/partition]# ./monthly_partition_manager.sh -d test -t range_columns_par -r month -a 3 -x 3 -f quote
 ------------------------------------------------------------------------------------------------------------
 -- Partition manager :: 2017-10-25 12:26:48
 --     Target DB    :: test
---     Target Table :: Clogs
+--     Target Table :: range_columns_par
 ------------------------------------------------------------------------------------------------------------
 Warning: Using a password on the command line interface can be insecure.
 ADD Partiton for 3 month(s)
 DROP Partition before 3 month(s)
 Warning: Using a password on the command line interface can be insecure.
 ADD PARTITION: p201712, '2018-01-01'
-ALTER TABLE test.Clogs ADD PARTITION (PARTITION p201712 VALUES LESS THAN ('2018-01-01'))
+ALTER TABLE test.range_columns_par ADD PARTITION (PARTITION p201712 VALUES LESS THAN ('2018-01-01'))
 Warning: Using a password on the command line interface can be insecure.
 DROP PARTITION: p201706, '2017-07-01'
-ALTER TABLE test.Clogs DROP PARTITION p201706
+ALTER TABLE test.range_columns_par DROP PARTITION p201706
 Warning: Using a password on the command line interface can be insecure.
 ```
--- my_partition_manager.sh
+++ my_partition_manager.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
-## -------------------------------------------------------------------------------------
-## Auto partition manager
-## -------------------------------------------------------------------------------------
-## -- crontab :
-## ## Auto parition manager script
-## ## 0 3 * * *  sh /mysql/admin/partition/monthly_partition_manager.sh -d dbname -t tablename -x 3 -a 10 > /mysql/admin/partition/monthly_partition_dbname_tablename.log  2>&1
-## -------------------------------------------------------------------------------------
-
+# range partition : test.userlogs
 usage() {
-    echo "Usage: $0 -d <dbname> -t <tablename> [-x <expire_months>] [-a <add_months>] [-p <partition_prefix>] [-f <partitioning_key_function>]"
+    echo "Usage: $0 -d <dbname> -t <tablename> -r month|day|week [-x <expire_unit>] [-a <add_unit>] [-p <partition_prefix>] [-f <partitioning_key_function>]"
+    echo "An expire/add unit is based on your range type. " 
     exit 1
 }
 
 PAR_PREFIX='p'
 PAR_FUNC='UNIX_TIMESTAMP'
-while getopts ":d:t:x:a:p:f:" arg; do
+while getopts ":d:t:r:x:a:p:f:" arg; do
   case $arg in
       d) DB_NAME=${OPTARG}
          ;;
       t) TABLE_NAME=${OPTARG}
          ;;
-      x) EXPIRE_MONTHS=${OPTARG}
-         if [ $EXPIRE_MONTHS -lt 1 ] ; then echo "EXPIRE_MONTHS needs to be greater than or equal to 1."; usage; fi
+      r) RANGE_TYPE=${OPTARG}
+         if [ $RANGE_TYPE != "month" -a $RANGE_TYPE != "day" -a $RANGE_TYPE != "week" ] ; then echo "ERROR IN OPTION: Please choose month, day, or week for your RANGE_TYPE"; usage; fi
          ;;
-      a) ADD_MONTHS=${OPTARG}
+      x) EXPIRE_UNITS=${OPTARG}
+         if [ $EXPIRE_UNITS -lt 1 ] ; then echo "ERROR IN OPTION: EXPIRE_UNITS needs to be greater than or equal to 1."; usage; fi
+         ;;
+      a) ADD_UNITS=${OPTARG}
          ;;
       p) PAR_PREFIX=${OPTARG}
          ;;
@@ -38,12 +35,12 @@ while getopts ":d:t:x:a:p:f:" arg; do
   esac
 done
 
-if [ -z "$DB_NAME" -o -z "$TABLE_NAME" ]; then
-    echo "DB_NAME, TABLE_NAME are mandatory"
+if [ -z "$DB_NAME" -o -z "$TABLE_NAME" -o -z "$RANGE_TYPE" ]; then
+    echo "DB_NAME, TABLE_NAME, RANGE_TYPE are mandatory"
     usage
 fi
 
-if [ -z "$EXPIRE_MONTHS"  -a -z "$ADD_MONTHS" ]; then
+if [ -z "$EXPIRE_UNITS"  -a -z "$ADD_UNITS" ]; then
     echo "Please select at least one task (expire or add)"
     usage; #exit 1;
 fi
@@ -51,9 +48,19 @@ fi
 
 MYSQL_HOME=/mysql/MyHome
 MYSQL_HOST=127.0.0.1
-MYSQL_PORT=3306
+MYSQL_PORT=20306
 MYSQL_USER=root
 MYSQL_PASS='xxxx'
+
+
+## -------------------------------------------------------------------------------------
+## Auto partition manager
+## -------------------------------------------------------------------------------------
+## -- crontab :
+## ## Auto parition manager script
+## ## 0 3 * * *  sh /mysql/admin/partition/my_partition_manager.sh -d dbname -t tablename -r monthly -x 3 -a 4 > /mysql/admin/partition/monthly_partition_dbname_tablename.log  2>&1
+## ## 0 3 * * *  sh /mysql/admin/partition/my_partition_manager.sh -d dbname -t tablename -r daily -x 90 -a 91 > /mysql/admin/partition/daily_partition_dbname_tablename.log  2>&1
+## -------------------------------------------------------------------------------------
 
 ## -------------------------------------------------------------------------------------
 ## Install script base directory
@@ -61,13 +68,12 @@ SCRIPT_BASEDIR=$(dirname $0)
 ## -------------------------------------------------------------------------------------
 
 ## This call will terminate script execution if there's error
-## Call : exit_if_error $? "error message"
+## Call : exit_if_error $? "can't get mysql replication role"
 ## -------------------------------------------------------------------------------------
 function exit_if_error(){
   if [ $1 -ne 0 ] ; then
     CURRENT_DTTM=`date '+%Y-%m-%d %H:%M:%S'`
     echo "[$CURRENT_DTTM][ERROR] PartitionManager: $2"
-    # Customize your error handling block
     exit
   fi
 }
@@ -77,6 +83,7 @@ echo "--------------------------------------------------------------------------
 echo "-- Partition manager :: ${CURRENT_DTTM}"
 echo "--     Target DB    :: ${DB_NAME}"
 echo "--     Target Table :: ${TABLE_NAME}"
+echo "--     Range Type :: ${RANGE_TYPE}"
 echo "------------------------------------------------------------------------------------------------------------"
 
 ## CHECK TABLE IS PARTITION TABLE
@@ -85,15 +92,26 @@ PARTITION_NOT_EXISTS=`${MYSQL_HOME}/bin/mysql --user="${MYSQL_USER}" --password=
 #echo $PARTITION_NOT_EXISTS
 exit_if_error ${PARTITION_NOT_EXISTS} "Partition Table does not exists: <${PARTITION_NOT_EXISTS}> $QUERY_TO_CHECK"
 
+## RANGE_TYPE and Variables 
+if [ $RANGE_TYPE = 'month' ]; then
+	expr_to_create="DATE_FORMAT(cal.date - INTERVAL 1 MONTH, '%Y%m')"
+	expr_high="DATE_FORMAT(cal.date,'%Y-%m-01')"
+elif [ $RANGE_TYPE = 'week' ]; then
+	expr_to_create="DATE_FORMAT(cal.date - interval 1 WEEK - interval weekday(cal.date) DAY, '%Y%m%d')"
+	expr_high="DATE_FORMAT(cal.date  - interval weekday(cal.date) day,'%Y-%m-%d')"
+elif [ $RANGE_TYPE = 'day' ]; then
+	expr_to_create="DATE_FORMAT(cal.date - interval 1 DAY, '%Y%m%d')"
+	expr_high="DATE_FORMAT(cal.date,'%Y-%m-%d')"
+fi
 
 ## CHECK PARTITION TO CREATE OR DROP
-if [ $ADD_MONTHS ]; then
-  echo "ADD Partiton for $ADD_MONTHS month(s)"
+if [ $ADD_UNITS ]; then
+  echo "ADD Partiton for $ADD_UNITS $RANGE_TYPE(s)"
   QUERY_TO_GET_PARTITION_TO_ADD="
--- get partitions to add (ADD_MONTHS)
-SELECT CONCAT('${PAR_PREFIX}',DATE_FORMAT(cal.date - INTERVAL 1 MONTH, '%Y%m')) as part_to_create, ${PAR_FUNC}(DATE_FORMAT(cal.date,'%Y-%m-01')) as part_high, t.partition_name, t.partition_description
+-- get partitions to add (ADD_UNITS)
+SELECT CONCAT('${PAR_PREFIX}',$expr_to_create) as part_to_create, ${PAR_FUNC}($expr_high) as part_high, t.partition_name, t.partition_description
  FROM (
-      SELECT SUBDATE(NOW(), INTERVAL ${EXPIRE_MONTHS}-1 MONTH) + INTERVAL xc MONTH AS date
+      SELECT SUBDATE(NOW(), INTERVAL ${EXPIRE_UNITS}-1 $RANGE_TYPE) + INTERVAL xc $RANGE_TYPE AS date
       FROM (
             SELECT @xi:=@xi+1 as xc from
             (SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4) xc1,
@@ -105,30 +123,30 @@ SELECT CONCAT('${PAR_PREFIX}',DATE_FORMAT(cal.date - INTERVAL 1 MONTH, '%Y%m')) 
 ) cal
 LEFT JOIN
 (select partition_name, partition_description from information_schema.partitions where table_schema='${DB_NAME}' and table_name='${TABLE_NAME}') t
-on ${PAR_FUNC}(DATE_FORMAT(cal.date,'%Y-%m-01')) = t.partition_description
+on ${PAR_FUNC}($expr_high) = t.partition_description
 "
 #### WHERE CONDITION, SUPPORT RANGE COLUMNS
   if [ ${PAR_FUNC^^} == 'QUOTE' ]; then
     QUERY_TO_GET_PARTITION_TO_ADD+="
-where t.partition_name IS NULL AND (DATE_FORMAT(cal.date,'%Y-%m-01')) between  (select str_to_date(max(partition_description),'\'%Y-%m-%d\'') as max_value FROM information_schema.partitions  WHERE table_schema='${DB_NAME}' AND table_name='${TABLE_NAME}') and (NOW() + INTERVAL ${ADD_MONTHS} MONTH)
+where t.partition_name IS NULL AND ($expr_high) between  (select str_to_date(max(partition_description),'\'%Y-%m-%d\'') as max_value FROM information_schema.partitions  WHERE table_schema='${DB_NAME}' AND table_name='${TABLE_NAME}') and (NOW() + INTERVAL ${ADD_UNITS} $RANGE_TYPE)
 ORDER BY 1, 2, 3
 "
   else
     QUERY_TO_GET_PARTITION_TO_ADD+="
-where t.partition_name IS NULL AND ${PAR_FUNC}(DATE_FORMAT(cal.date,'%Y-%m-01')) between  (select max(partition_description) as max_value FROM information_schema.partitions  WHERE table_schema='${DB_NAME}' AND table_name='${TABLE_NAME}') and ${PAR_FUNC}(NOW() + INTERVAL ${ADD_MONTHS} MONTH)
+where t.partition_name IS NULL AND ${PAR_FUNC}($expr_high) between  (select max(partition_description) as max_value FROM information_schema.partitions  WHERE table_schema='${DB_NAME}' AND table_name='${TABLE_NAME}') and ${PAR_FUNC}(NOW() + INTERVAL ${ADD_UNITS} $RANGE_TYPE)
 ORDER BY 1, 2, 3
 "
   fi
 #echo $QUERY_TO_GET_PARTITION_TO_ADD
 #GET_PARTITION_TO_ADD=`${MYSQL_HOME}/bin/mysql --user="${MYSQL_USER}" --password="${MYSQL_PASS}" --skip-column-names --batch --execute="${QUERY_TO_GET_PARTITION_TO_ADD}"`
 fi
-if [ $EXPIRE_MONTHS ]; then
-  echo "DROP Partition before $EXPIRE_MONTHS month(s)"
+if [ $EXPIRE_UNITS ]; then
+  echo "DROP Partition before $EXPIRE_UNITS $RANGE_TYPE(s)"
   QUERY_TO_GET_PARTITION_TO_DROP="
--- get partitions to drop (EXPIRE_MONTHS)
-SELECT CONCAT('${PAR_PREFIX}',DATE_FORMAT(cal.date - INTERVAL 1 MONTH, '%Y%m')) as part_to_create, ${PAR_FUNC}(DATE_FORMAT(cal.date,'%Y-%m-01')) as part_high, t.partition_name, t.partition_description
+-- get partitions to drop (EXPIRE_UNITS)
+SELECT CONCAT('${PAR_PREFIX}',$expr_to_create) as part_to_create, ${PAR_FUNC}($expr_high) as part_high, t.partition_name, t.partition_description
  FROM (
-SELECT SUBDATE(NOW(), INTERVAL ${EXPIRE_MONTHS}-1 MONTH) + INTERVAL xc MONTH AS date
+SELECT SUBDATE(NOW(), INTERVAL ${EXPIRE_UNITS}-1 $RANGE_TYPE) + INTERVAL xc $RANGE_TYPE AS date
 FROM (
       SELECT @xi:=@xi+1 as xc from
       (SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4) xc1,
@@ -140,17 +158,17 @@ FROM (
 ) cal
 RIGHT JOIN
 (select partition_name, partition_description from information_schema.partitions WHERE table_schema='${DB_NAME}' AND table_name='${TABLE_NAME}') t
-ON ${PAR_FUNC}(DATE_FORMAT(cal.date,'%Y-%m-01')) = t.partition_description
+ON ${PAR_FUNC}($expr_high) = t.partition_description
 "
 #### WHERE CONDITION, SUPPORT RANGE COLUMNS
   if [ ${PAR_FUNC^^} == 'QUOTE' ]; then
     QUERY_TO_GET_PARTITION_TO_DROP+="
-WHERE cal.date is null and str_to_date(t.partition_description,'\'%Y-%m-%d\'') < (SUBDATE(NOW(), INTERVAL ${EXPIRE_MONTHS}-1 MONTH))
+WHERE cal.date is null and str_to_date(t.partition_description,'\'%Y-%m-%d\'') < (SUBDATE(NOW(), INTERVAL ${EXPIRE_UNITS}-1 $RANGE_TYPE))
 ORDER BY 1, 2, 3
 "
   else
     QUERY_TO_GET_PARTITION_TO_DROP+="
-WHERE cal.date is null and t.partition_description < ${PAR_FUNC}(SUBDATE(NOW(), INTERVAL ${EXPIRE_MONTHS}-1 MONTH))
+WHERE cal.date is null and t.partition_description < ${PAR_FUNC}(SUBDATE(NOW(), INTERVAL ${EXPIRE_UNITS}-1 $RANGE_TYPE))
 ORDER BY 1, 2, 3
 "
   fi
@@ -172,7 +190,7 @@ do
 	  QUERY_ADD_PARTITION="ALTER TABLE \`${DB_NAME}\`.\`${TABLE_NAME}\` ADD PARTITION (PARTITION ${results[0]} VALUES LESS THAN (${results[1]}))"
 	  echo $QUERY_ADD_PARTITION
 	  ${MYSQL_HOME}/bin/mysql --user="${MYSQL_USER}" --password="${MYSQL_PASS}" --execute="${QUERY_ADD_PARTITION}"
-	  exit_if_error $? "Add partition query failed: <$?> $QUERY_ADD_PARTITION"
+          exit_if_error $? "Add partition query failed: <$?> $QUERY_ADD_PARTITION"
   elif [ ${results[0]} == "NULL" ] && [ ${results[2]} != "NULL" ];then
 	  echo "DROP PARTITION: ${results[2]}, ${results[3]}"
 	  QUERY_DROP_PARTITION="ALTER TABLE \`${DB_NAME}\`.\`${TABLE_NAME}\` DROP PARTITION ${results[2]}"
@@ -181,4 +199,5 @@ do
 	  exit_if_error $? "Drop partition query failed : <$?> $QUERY_DROP_PARTITION"
 
   fi
+  #declare -p results
 done
